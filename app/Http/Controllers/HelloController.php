a0a40a8c381a6e0ab05a1abe4876a0710d86b95b
kotaro_k <kotaro.k13@gmail.com>
2019-01-31 14:21:32 +0900

    update to use request and response in hello-controller

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -3,43 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-global $head, $style, $body, $end;
-$head = '<html><head>';
-$style = <<<EOF
-<style>
-body {font-size:16pt; color:#999; }
-h1 {font-size:100pt; text-align:right; color:#eee;
-    margin:-40px 0px -50px 0px; }
-</style>
-EOF;
-$body = '</head><body>';
-$end = '</body></head>';
-
-function tag($tag, $txt) {
-    return "<{$tag}>" . $txt . "</{$tag}>";
-}
+use Illuminate\Http\Response; //add one
 
 class HelloController extends Controller
 {
-    public function index(){
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title','Hello/Index') . $style
-                . $body
-                . tag('h1', 'Index') . tag('p', 'this is Index page')
-                . '<a href = "/hello/other">go to Other page</a>'
-                . $end;
-        return $html;
-    }
-
-    public function other() {
-        global $head, $style, $body, $end;
-
-        $html = $head . tag('title','Hello/Other') . $style
-        . $body
-        . tag('h1', 'Other') . tag('p', 'this is Other page')
-        . $end;
-        return $html;
+    public function index(Request $request, Response $response){
+        $html = <<<EOF
+        <html>
+        <head>
+        <title>Hello/Index</title>
+        <style>
+            body {font-size:16pt; color:#999; }
+            h1 {font-size:120pt; text-align:right; color:#fafafa;
+            margin:-50px 0px -120px 0px; }
+        </style>
+        </head>
+        <body>
+            <h1>Hello</h1>
+            <h3>Request</h3>
+            <pre>{$request}</pre>
+            <h3>Response</h3>
+            <pre>{$response}</pre>
+        </body>
+        </html>
+EOF;
+        $response -> setContent($html);
+        return $response;
     }
 }
+
+// class HelloController extends Controller
+// {
+//     public function __invoke(){
+//         return <<<EOF
+//         <html>
+//         <head>
+//         <title>Hello/Index</title>
+//         <style>
+//             body {font-size:16pt; color:#999; }
+//             h1 {font-size:100pt; text-align:right; color:#eee;
+//             margin:-40px 0px -50px 0px; }
+//         </style>
+//         </head>
+//         <body>
+//             <h1>Single Action</h1>
+//             <p>これは、シングルアクションコントローラのアクションです。</p>
+//         </body>
+//         </html>
+// EOF;
+//     }
+// }
+
+// class HelloController extends Controller
+// {
+//     public function index(){
+//         global $head, $style, $body, $end;
+
+//         $html = $head . tag('title','Hello/Index') . $style
+//                 . $body
+//                 . tag('h1', 'Index') . tag('p', 'this is Index page')
+//                 . '<a href = "/hello/other">go to Other page</a>'
+//                 . $end;
+//         return $html;
+//     }
+
+//     public function other() {
+//         global $head, $style, $body, $end;
+
+//         $html = $head . tag('title','Hello/Other') . $style
+//         . $body
+//         . tag('h1', 'Other') . tag('p', 'this is Other page')
+//         . $end;
+//         return $html;
+//     }
+// }
